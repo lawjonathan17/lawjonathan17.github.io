29cf156dda0306e5cb873567c465322182b1c464
Jonathan Law <lawjonathan17@gmail.com>
2017-11-25 17:42:03 -0800

    Permanent nav bar and background images
    This patch adds a permanent nav bar for desktop and tablet viewers. This patch also adds background images for the introduction statement.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
 // Back to top button JS
 // Code referenced from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
 // When the user scrolls down 100px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() { scrollFunction() };
 
-function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+function scrollFunction()
+{
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100)
+    {
         document.getElementById("back-to-top-button").style.display = "block";
-    } else {
+    }
+    else
+    {
         document.getElementById("back-to-top-button").style.display = "none";
     }
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+function topFunction()
+{
     document.body.scrollTop = 0; // For Chrome, Safari and Opera 
     document.documentElement.scrollTop = 0; // For IE and Firefox
 }
+
